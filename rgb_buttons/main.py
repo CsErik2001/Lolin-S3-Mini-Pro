11c5b19f29bbceeb585b2a22138e37b763f87812
Erik <cserik2001@gmail.com>
2024-08-17 15:24:47 +0200

    rgb buttons v2

--- rgb_buttons/main.py
+++ rgb_buttons/main.py
@@ -1,34 +1,43 @@
-from machine import Pin
+# Import necessary modules from the helper library
 from time import sleep
-from neopixel import NeoPixel
+import s3minipro as helper  # Replace 's3minipro' with the actual filename
 
-# Set up the RGB LED power pin
-power_pin = Pin(7, Pin.OUT)
-power_pin.value(1)  # Ensure the RGB LED is powered
-
-# Set up the RGB LED data pin
-led_pin = Pin(8, Pin.OUT)
-np = NeoPixel(led_pin, 1)
-
-# Set up the buttons
-button1 = Pin(0, Pin.IN, Pin.PULL_UP)
-button2 = Pin(47, Pin.IN, Pin.PULL_UP)
-button3 = Pin(48, Pin.IN, Pin.PULL_UP)
+# Ensure RGB LED power is enabled
+power_pin = Pin(helper.RGB_POWER, Pin.OUT)
+power_pin.value(1)  # Turn on the RGB LED power
 
 while True:
-    if not button1.value():  # Button 1 pressed
-        np[0] = (0, 0, 255)  # Red
-        np.write()
-        print("Button 1 pressed: Red")
+    # Check for all possible combinations of button presses
+    if not helper.button0.value() and not helper.button47.value() and not helper.button48.value():  # All three buttons pressed
+        helper.rgb_led(255, 255, 255)  # White (Red + Green + Blue)
+        print("Button 0 (Blue), Button 47 (Red), and Button 48 (Green) pressed: White")
 
-    elif not button2.value():  # Button 2 pressed
-        np[0] = (0, 255, 0)  # Green
-        np.write()
-        print("Button 2 pressed: Green")
+    elif not helper.button0.value() and not helper.button47.value():  # Blue and Red buttons pressed together
+        helper.rgb_led(255, 0, 255)  # Magenta (Red + Blue)
+        print("Button 0 (Blue) and Button 47 (Red) pressed: Magenta")
 
-    elif not button3.value():  # Button 3 pressed
-        np[0] = (255, 0, 0)  # Blue
-        np.write()
-        print("Button 3 pressed: Blue")
+    elif not helper.button0.value() and not helper.button48.value():  # Blue and Green buttons pressed together
+        helper.rgb_led(0, 255, 255)  # Cyan (Green + Blue)
+        print("Button 0 (Blue) and Button 48 (Green) pressed: Cyan")
+
+    elif not helper.button47.value() and not helper.button48.value():  # Red and Green buttons pressed together
+        helper.rgb_led(255, 255, 0)  # Yellow (Red + Green)
+        print("Button 47 (Red) and Button 48 (Green) pressed: Yellow")
+
+    elif not helper.button0.value():  # Button 0 pressed (Blue)
+        helper.rgb_led(0, 0, 255)  # Blue
+        print("Button 0 (Blue) pressed: Blue")
+
+    elif not helper.button47.value():  # Button 47 pressed (Red)
+        helper.rgb_led(255, 0, 0)  # Red
+        print("Button 47 (Red) pressed: Red")
+
+    elif not helper.button48.value():  # Button 48 pressed (Green)
+        helper.rgb_led(0, 255, 0)  # Green
+        print("Button 48 (Green) pressed: Green")
+
+    else:
+        helper.rgb_led(0, 0, 0)  # Turn off the LED if no buttons are pressed
+        print("No button pressed: LED off")
 
     sleep(0.1)  # Debounce delay
